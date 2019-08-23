055fee111f0d74ea1ac07bcd33091af9f6006381
LiuQian <liuqian@flabbj.com>
2019-08-23 18:43:37 +0800

    登录页面

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -1,11 +1,16 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:yoozflutter_mendian/main_home.dart';
+import 'package:yoozflutter_mendian/utils/ToastUtils.dart';
 
 //登录
 class LoginPager extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: InPutWidget());
+    return MaterialApp(
+        home: Scaffold(
+            body: InPutWidget(), resizeToAvoidBottomPadding: false) //防止软键盘叮咚布局
+        );
   }
 }
 
@@ -25,7 +30,8 @@ class _InPutWidgetState extends State<InPutWidget> {
   @override
   void initState() {
     super.initState();
-    _emailController.value = _emailController.value.copyWith(text: "test@yoozworld.com");
+    _emailController.value =
+        _emailController.value.copyWith(text: "test@yoozworld.com");
     _passController.value = _passController.value.copyWith(text: "123456");
   }
 
@@ -33,56 +39,126 @@ class _InPutWidgetState extends State<InPutWidget> {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(16.0),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            createRowInputTextField(
-                textField: createInputTextField(false),
-                rightImage: GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      _emailController.value = _emailController.value.copyWith(text: "");
-                    });
-                  },
+      child: Stack(
+        alignment: AlignmentDirectional.topCenter,
+        children: <Widget>[
+          Positioned(
+            top: 120.0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 50.0),
                   child: Image.asset(
-                    'assets/images/user_input_delete.png',
-                    width: 16.0,
-                    height: 16.0,
+                    'assets/images/user_login_logo.png',
+                    width: 180.0,
+                    height: 46.0,
                   ),
                 ),
-                marginTop: 0),
-            createRowInputTextField(
-                textField: createInputTextField(true),
-                rightImage: createPassImage(),
-                marginTop: 16.0),
-            Padding(
-                //登录按钮
-                padding: const EdgeInsets.only(top: 34.0),
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: RaisedButton(
-                        padding: const EdgeInsets.all(8.0),
-                        color: Colors.blue,
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) {
-                                    return MainPager();
-                                  },
-                                  maintainState: false));
-                        },
-                        child: Text('登录',
-                            style:
-                                TextStyle(fontSize: 18.0, color: Colors.white)),
+                createRowInputTextField(
+                    textField: createInputTextField(false),
+                    rightImage: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _emailController.value =
+                              _emailController.value.copyWith(text: "");
+                        });
+                      },
+                      child: Image.asset(
+                        'assets/images/user_input_delete.png',
+                        width: 16.0,
+                        height: 16.0,
                       ),
                     ),
+                    marginTop: 0),
+                createRowInputTextField(
+                    textField: createInputTextField(true),
+                    rightImage: createPassImage(),
+                    marginTop: 16.0),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10.0),
+                  child: Row(
+                    textDirection: TextDirection.rtl,
+                    children: <Widget>[
+                      GestureDetector(
+                        onTap: () {
+                          showToast('修改密码');
+                        },
+                        child: Text(
+                          '忘记密码',
+                          style: TextStyle(color: Colors.blue, fontSize: 14.0),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Padding(
+                    //登录按钮
+                    padding: const EdgeInsets.only(
+                        top: 30.0, left: 10.0, right: 10.0),
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: RaisedButton(
+                            shape: RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(8.0))),
+                            padding: const EdgeInsets.all(8.0),
+                            color: Colors.blue,
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) {
+                                        return MainPager();
+                                      },
+                                      maintainState: false));
+                            },
+                            child: Text('登录',
+                                style: TextStyle(
+                                    fontSize: 18.0, color: Colors.white)),
+                          ),
+                        ),
+                      ],
+                    )),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 0.0,
+            child: Row(
+              children: <Widget>[
+                Wrap(
+                  children: <Widget>[
+                    Text(
+                      '点击登录，即表示已阅读并同意',
+                      style:
+                          TextStyle(color: Color(0xFF9B9B9B), fontSize: 12.0),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        showToast('YOOZ用户协议');
+                      },
+                      child: Text(
+                        'YOOZ用户协议',
+                        style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 12.0,
+                            decoration: TextDecoration.underline,
+                            decorationColor: Colors.blue,
+                            decorationStyle: TextDecorationStyle.solid),
+                      ),
+                    )
                   ],
-                )),
-          ],
-        ),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
@@ -99,7 +175,10 @@ class _InPutWidgetState extends State<InPutWidget> {
         textAlignVertical: TextAlignVertical.center,
         keyboardType: TextInputType.number,
         style: TextStyle(fontSize: 16.0),
-        decoration: InputDecoration(hintText: "输入密码", border: InputBorder.none),
+        decoration: InputDecoration(
+            hintText: "输入密码",
+            hintStyle: TextStyle(color: Color(0xFF9B9B9B)),
+            border: InputBorder.none),
         scrollPadding: const EdgeInsets.all(10.0),
       );
     } else {
@@ -111,7 +190,10 @@ class _InPutWidgetState extends State<InPutWidget> {
         textAlignVertical: TextAlignVertical.center,
         keyboardType: TextInputType.emailAddress,
         style: TextStyle(fontSize: 16.0),
-        decoration: InputDecoration(hintText: "输入账号", border: InputBorder.none),
+        decoration: InputDecoration(
+            hintText: "输入账号",
+            hintStyle: TextStyle(color: Color(0xFF9B9B9B)),
+            border: InputBorder.none),
         scrollPadding: const EdgeInsets.all(10.0),
       );
     }
@@ -157,9 +239,13 @@ class _InPutWidgetState extends State<InPutWidget> {
             decoration: BoxDecoration(
                 border: BoxBorder.lerp(
                     Border.all(
-                        color: Colors.blue, width: 1, style: BorderStyle.solid),
+                        color: Color(0xFFECECEC),
+                        width: 1,
+                        style: BorderStyle.solid),
                     Border.all(
-                        color: Colors.blue, width: 1, style: BorderStyle.solid),
+                        color: Color(0xFFECECEC),
+                        width: 1,
+                        style: BorderStyle.solid),
                     1),
                 borderRadius: BorderRadius.all(Radius.circular(6.0))),
             child: Padding(
--- lib/utils/ToastUtils.dart
+++ lib/utils/ToastUtils.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
-showToast(String msg) {
+showToast(String msg, {Color backgroundColor = Colors.blue}) {
   Fluttertoast.showToast(
       msg: msg,
       toastLength: Toast.LENGTH_LONG,
       gravity: ToastGravity.CENTER,
-      backgroundColor: Colors.blue,
+      backgroundColor: backgroundColor,
       textColor: Colors.white);
 }
